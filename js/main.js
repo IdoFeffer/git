f7362fa70557cdc5a189a940caff522a1d5086df
Ido Feffer <idoppr1@gmail.com>
2025-03-18 14:17:33 +0200

    Update ball1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 'use strict'
 
+var ball1Size = 100
+
 function onBallClick(event){
-    console.log('Hello');
+    var elBall = document.querySelector('.ball1')
+    ball1Size = ball1Size + 50
+    
+    elBall.innerText = ball1Size
+    elBall.style.width = ball1Size + 'px'
+    elBall.style.height = ball1Size + 'px'
 }
