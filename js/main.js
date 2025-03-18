727d3a5efb16772c14389bcaf969a47c1efc4400
Ido Feffer <idoppr1@gmail.com>
2025-03-18 18:20:15 +0200

    Add onBall3Click()

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
 'use strict'
 
-// var ball1Size = 100
+const gBalls = document.querySelectorAll('.ball')
+for (var i = 0; i < gBalls.length; i++){
+    gBalls[i].style.backgroundColor = getRandomColor()
+}
+
 
 function onBallClick(elBall, maxDiameter){
     var ballSize = elBall.offsetWidth
     console.log(ballSize);
-    // var ballWidth = elBall.style.width
-    // var ballHeight = elBall.style.height
     const randomSize = getRandomInt(20, 60) 
-    // console.log(randomSize);
-    // console.log(elBall);
-    // // var elBall = document.querySelector('.ball1')
     ballSize  += randomSize
-    // // ballWidth +=  randomSize
-    // // ballHeight += randomSize
     if (ballSize > maxDiameter) {
         ballSize = 100
-        // ballWidth = 100
-        // ballHeight = 100
     }
     elBall.innerText = ballSize
     elBall.style.width = ballSize + 'px'
@@ -25,3 +20,22 @@ function onBallClick(elBall, maxDiameter){
     elBall.style.backgroundColor = getRandomColor()
 }
 
+function onBall3Click(){
+    var ball1 = gBalls[0]
+    var ball2 = gBalls[1]
+
+    console.dir(ball1)
+    var colorBall1 = ball1.style.backgroundColor
+    var tempWidth = ball1.offsetWidth
+    console.log(colorBall1);
+
+    ball1.style.backgroundColor = ball2.style.backgroundColor
+    ball1.style.width = ball2.offsetWidth + 'px'
+    ball1.style.height = ball2.offsetWidth + 'px'
+    ball1.innerText = ball1.offsetWidth
+
+    ball2.style.backgroundColor = colorBall1
+    ball2.style.width = tempWidth + 'px'
+    ball2.style.height = tempWidth + 'px'
+    ball2.innerText = ball2.offsetWidth
+}
