482c52ad2b7812401dc4f74e4a2f74fba974ddc2
Ido Feffer <idoppr1@gmail.com>
2025-03-18 15:04:50 +0200

    Change ball1 Size

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ var ball1Size = 100
 function onBallClick(event){
     var elBall = document.querySelector('.ball1')
     ball1Size = ball1Size + 50
+    if (ball1Size > 400) ball1Size = 100
     
     elBall.innerText = ball1Size
     elBall.style.width = ball1Size + 'px'
     elBall.style.height = ball1Size + 'px'
+
 }
