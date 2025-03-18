e1b6ccb992ab38d16c1d87f488ed53821f0311e1
Ido Feffer <idoppr1@gmail.com>
2025-03-18 16:05:01 +0200

    Added util file and change ball color

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ var ball1Size = 100
 
 function onBallClick(event){
     var elBall = document.querySelector('.ball1')
-    ball1Size = ball1Size + 50
+    ball1Size = ball1Size + getRandomInt(20, 60)
     if (ball1Size > 400) ball1Size = 100
     
     elBall.innerText = ball1Size
     elBall.style.width = ball1Size + 'px'
     elBall.style.height = ball1Size + 'px'
-
+    elBall.style.backgroundColor = getRandomColor()
 }
